07c9d12c8cfb02ae4a99d03e17cdebec6434e351
ngthngoc <nguyenthengoc.782k4@gmail.com>
2026-03-20 04:37:23 +0700

    feat(seeding): implement seeders for admin, users, movies, plans, and showtimes

---
+++ Mv.Infrastructure/Persistence/Configurations/Domain/MovieConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Mv.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mv.Infrastructure.Persistence.Configurations.Domain;
+
+public class MovieConfiguration : BaseConfiguration<Movie> {
+  public override void Configure(EntityTypeBuilder<Movie> builder) {
+    base.Configure(builder);
+
+    builder.Property(m => m.Name).IsRequired().HasMaxLength(255);
+    builder.Property(m => m.PosterUrl).IsRequired().HasMaxLength(1000);
+  }
+}
