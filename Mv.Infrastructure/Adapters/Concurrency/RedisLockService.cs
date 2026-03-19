bc2fe2a32657e7956e1d87925809e82346204137
VuPhong258 <trannguyenvuphong258@gmail.com>
2026-03-19 23:34:58 +0700

    feat(infra): implement distributed lock service with redis

--- Mv.Infrastructure/Adapters/Concurrency/RedisLockService.cs
+++ Mv.Infrastructure/Adapters/Concurrency/RedisLockService.cs
@@ -1,9 +1,15 @@
-﻿using Mv.Application.Ports.Concurrency;
+﻿using Medallion.Threading;
+using Mv.Application.Ports.Concurrency;
 
 namespace Mv.Infrastructure.Adapters.Concurrency;
 
-public class RedisLockService : IDistributedLockService {
-  public Task<IDisposable?> AcquireLockAsync(string resourceKey, TimeSpan wait) {
-    throw new NotImplementedException();
+public class RedisLockService(IDistributedLockProvider lockProvider) : IDistributedLockService {
+  public async Task<IDisposable?> AcquireLockAsync(string resourceKey, TimeSpan wait) {
+    try {
+      var handle = await lockProvider.TryAcquireLockAsync(resourceKey, wait);
+      return handle;
+    } catch {
+      return null;
+    }
   }
 }
