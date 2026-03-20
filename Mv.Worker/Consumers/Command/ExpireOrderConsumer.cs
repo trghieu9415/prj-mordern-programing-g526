8619ddf956b058924cd5bc1cf8bdc3722508e55f
ngthngoc <nguyenthengoc.782k4@gmail.com>
2026-03-20 19:40:29 +0700

    feat(external): integrate third-party services  including Stripe, PayPal, S3, email, Redis, RabbitMQ and Quartz

---
+++ Mv.Worker/Consumers/Command/ExpireOrderConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using MediatR;
+using Mv.Application.UseCases.System.ExpireOrder;
+
+namespace Mv.Worker.Consumers.Command;
+
+public class ExpireOrderConsumer(
+  IMediator mediator
+) : IConsumer<ExpireOrderCommand> {
+  public async Task Consume(ConsumeContext<ExpireOrderCommand> context) {
+    var command = context.Message;
+    await mediator.Send(command, context.CancellationToken);
+  }
+}
---
+++ Mv.Worker/Consumers/Event/PaymentCompletedConsumer.cs
@@ -0,0 +1,28 @@
+﻿using Mv.Domain.Events;
+using MassTransit;
+using MediatR;
+using Mv.Application.Constants;
+using Mv.Application.Ports.Realtime;
+using Mv.Application.UseCases.System.MarkOrderAsPaid;
+
+namespace Mv.Worker.Consumers.Event;
+
+public class PaymentCompletedConsumer(
+  IMediator mediator,
+  IUserNotifier userNotifier
+) : IConsumer<PaymentCompletedEvent> {
+  public async Task Consume(ConsumeContext<PaymentCompletedEvent> context) {
+    var msg = context.Message;
+    var command = new MarkOrderAsPaidCommand(msg.OrderId);
+    await mediator.Send(command, context.CancellationToken);
+
+    await userNotifier.SendToUser(
+      msg.CustomerId,
+      ClientMethods.PaymentSuccess,
+      new {
+        orderId = msg.OrderId,
+        amount = msg.Amount
+      }
+    );
+  }
+}
